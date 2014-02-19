490e68061ff46425eec0aec10d5a0d2b2311e8a0
Christina Taggart & Ricardo A. Rubio <pair+christina-taggart.rickarubio@devbootcamp.com>
2014-02-19 10:39:18 -0800

    initial commit

---
+++ tdd_hacker_news/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddHackerNews::Application.config.secret_token = '018e0dcfd6455db3891609fd167fa13362865f71b0ca827430a0a84f8dd3f5e39315529b28099906995d287f0af86810d489547e206a30ab34e55951153649d2'
